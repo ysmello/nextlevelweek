e869a551a60c1054449c2ec8beceeba3bb91ddf8
Yuri Mello <yuridemeello@gmail.com>
2020-06-09 23:37:36 -0300

    Funcionalidades: Cadastro de ponto de coleta, lista os itens de coleta, listar pontos por filtros, listar um ponto específico

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  console.log("Hello World !!");
-});
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
+routes.get('/points', pointsController.index);
 
 export default routes;
