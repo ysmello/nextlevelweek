b8467c9ff4e70348c0c04016cc7a5106b7ee80a7
Yuri Mello <yuridemeello@gmail.com>
2020-06-11 02:07:48 -0300

    Atualizando seed items

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -2,13 +2,13 @@ import Knex from "knex";
 
 export async function seed(knex: Knex) {
   await knex("items").insert([
-    { title: "Lâmpadas", image: "lampadas.svg" },
-    { title: "Pilhas e Baterias", image: "baterias.svg" },
-    { title: "Papéis e Papelão", image: "papeis-papelao.svg" },
-    { title: "Resíduos Orgânicos", image: "organicos.svg" },
-    { title: "Óleo de Cozinha", image: "oleo.svg" },
-    { title: "Eletrónicos", image: "eletronicos.svg" }
+    { id: 1, title: "Lâmpadas", image: "lampadas.svg" },
+    { id: 2, title: "Pilhas e Baterias", image: "baterias.svg" },
+    { id: 3, title: "Papéis e Papelão", image: "papeis-papelao.svg" },
+    { id: 4, title: "Resíduos Orgânicos", image: "organicos.svg" },
+    { id: 5, title: "Óleo de Cozinha", image: "oleo.svg" },
+    { id: 6, title: "Eletrónicos", image: "eletronicos.svg" }
   ]);
 
-  //await knex("items").del()
+  //await knex("items").del();
 }
