161625d28e5b9f24e40207538b26c8f199047cbe
Yuri Mello <yuridemeello@gmail.com>
2020-06-11 02:36:51 -0300

    Templates e cadatro de pontos de coleta finalizados

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import Routes from "./routes";
+
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
