1339c81931f776e873f59f2095943232aaa9cea3
Yuri Mello <yuridemeello@gmail.com>
2020-06-10 22:15:08 -0300

    Aplicando css e fonts iniciais

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
